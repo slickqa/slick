42e51ffd4f80edc97bbbe37919f7392426600f82
Jason Corbett <jason.corbett@vivint.com>
2018-02-20 13:08:36 -0700

    Many improvements to look and feel

--- web/src/js/App.js
+++ web/src/js/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 
+import PropTypes from 'prop-types';
 import App from 'grommet/components/App';
 import Split from 'grommet/components/Split';
+import Title from 'grommet/components/Title';
 import Box from 'grommet/components/Box';
 import Headline from 'grommet/components/Headline';
 import Button from 'grommet/components/Button';
@@ -11,9 +13,11 @@ import Footer from 'grommet/components/Footer';
 import Card from 'grommet/components/Card';
 import Section from 'grommet/components/Section';
 import Heading from 'grommet/components/Heading';
+import Header from 'grommet/components/Header';
 import Accordian from 'grommet/components/Accordion';
 import AccordianPanel from 'grommet/components/AccordionPanel';
 import Paragraph from 'grommet/components/Paragraph';
+import Select from 'grommet/components/Select';
 import UsersApi from 'slick-client/src/api/UsersApi';
 import VersionApi from 'slick-client/src/api/VersionApi';
 
@@ -21,19 +25,7 @@ function isLoggedIn() {
     return localStorage.token;
 }
 
-export class SlickLogo extends Component {
-  render() {
-    return (
-      <Box align="center">
-        <Headline size={"xlarge"} style={{"fontFamily": "Audiowide, cursive", "letter-spacing": "30px"}}>S
-          <span style={{"fontSize": "70%"}}>LICK</span>
-        </Headline>
-      </Box>
-    );
-  }
-}
-
-export class LoginPage extends Component {
+export class SlickFooter extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -47,22 +39,63 @@ export class LoginPage extends Component {
       });
     }.bind(this));
   }
+
+  render() {
+    return <Footer pad="small">{this.state.VersionString}</Footer>;
+  }
+}
+
+
+export class SlickLogo extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    let className = "slick-logo-xlarge";
+    if(this.props.size) {
+      className = "slick-logo-" + this.props.size;
+    }
+    return <Title className={className}>S<span style={{"fontSize": "70%"}}>LICK</span></Title>;
+  }
+}
+
+SlickLogo.propTypes = {
+  size: PropTypes.oneOf(["xlarge", "medium"])
+};
+
+export class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
         <Box full="vertical" className="LoginPage">
           <Box flex="grow" justify="center" className="LoginPageLogo">
             <Box alignSelf="center" align="center">
-              <SlickLogo/>
+              <Box align="center">
+                <SlickLogo size="xlarge"/>
+              </Box>
               <Button icon={<PlatformGoogleIcon/>} href="/login/google" label="Login with Google"/>
             </Box>
           </Box>
-          <Footer>{this.state.VersionString}</Footer>
+          <SlickFooter/>
         </Box>
     );
   }
 }
 
-export class UserInfoPage extends Component {
+function logout() {
+  delete localStorage.token;
+  delete localStorage.userName;
+  delete localStorage.userFirstName;
+  delete localStorage.userFamilyName;
+  delete localStorage.userGender;
+  delete localStorage.userPicture;
+  window.location.reload();
+}
+
+
+export class UserInfo extends Component {
   constructor(props) {
     super(props);
     this.users = new UsersApi();
@@ -78,17 +111,6 @@ export class UserInfoPage extends Component {
       "user": undefined
     };
     window.users = this.users;
-  }
-
-  handleClick(e) {
-    e.preventDefault();
-    delete localStorage.token;
-    delete localStorage.userName;
-    delete localStorage.userFirstName;
-    delete localStorage.userFamilyName;
-    delete localStorage.userGender;
-    delete localStorage.userPicture;
-    window.location.reload();
   }
 
   render() {
@@ -116,23 +138,63 @@ export class UserInfoPage extends Component {
 
 
     }
-    return (
-      <Box full="vertical" pad="medium">
-        <Headline size="large">Welcome {localStorage.userFirstName}</Headline>
-        <Box margin={{vertical: "small"}}>
-          <Button onClick={this.handleClick} primary={true} label="Logout" />
-        </Box>
-        {user}
-      </Box>
-    )
+    return user;
   }
 }
+
+
+export class SlickHeader extends Component {
+  render() {
+    return <Header className="slick-header" pad="small" colorIndex="brand">
+      <SlickLogo size="medium"/>
+      <Box flex="grow"> </Box>
+      {this.props.children}
+      <Button onClick={logout} label="Logout" />
+    </Header>;
+  }
+}
+
+export class ThemeDemo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      background: "Light"
+    };
+    this.changeBackground = this.changeBackground.bind(this);
+  }
+
+  changeBackground(opts) {
+    this.setState(function() {
+      return {background: opts.option};
+    });
+  }
+
+  render() {
+    let colorIndex="light-1";
+    if(this.state.background === "Dark") {
+      colorIndex="grey-1";
+    } else if(this.state.background === "Light") {
+      colorIndex="light-1";
+    }
+    return <Box full={true} colorIndex={colorIndex}>
+      <SlickHeader>
+        <Select options={["Light", "Dark"]} value={this.state.background} onChange={this.changeBackground} />
+      </SlickHeader>
+      <Box flex="grow" pad="small">
+        <UserInfo/>
+      </Box>
+      <SlickFooter/>
+    </Box>;
+  }
+}
+
+
 
 export default class BasicApp extends Component {
   render() {
     let page = <LoginPage/>;
     if(isLoggedIn()) {
-      page = <UserInfoPage/>;
+      page = <ThemeDemo/>;
     }
     return (
       <App centered={false}>
