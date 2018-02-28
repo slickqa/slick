a1d95ebc8cce46af61955673599fbc59ce27fc11
Jason Corbett <jason.corbett@vivint.com>
2018-02-28 12:33:13 -0700

    Working on #17 and #18

---
+++ web/src/api.js
@@ -0,0 +1,11 @@
+
+
+import UsersApi from 'slick-client/src/api/UsersApi';
+import VersionApi from 'slick-client/src/api/VersionApi';
+import CompanyApi from 'slick-client/src/api/CompanyApi';
+import AuthApi from 'slick-client/src/api/AuthApi';
+import ApiClient from 'slick-client/src/ApiClient';
+
+
+
+
