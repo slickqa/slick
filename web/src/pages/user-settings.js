4580738ff791b873c21da883035a84b7b8f96dd9
lee.higginson <lee.higginson@vivint.com>
2018-03-05 14:10:46 -0700

    Theme change is working removed accordian from user page

--- web/src/pages/user-settings.js
+++ web/src/pages/user-settings.js
@@ -1,16 +1,15 @@
 import React, {Component} from 'react';
 import Heading from 'grommet/components/Heading';
 import Columns from 'grommet/components/Columns';
-import Paragraph from 'grommet/components/Paragraph';
 import Image from 'grommet/components/Image';
 import Box from 'grommet/components/Box';
-import Accordion from 'grommet/components/Accordion';
-import AccordionPanel from 'grommet/components/AccordionPanel';
 import Label from 'grommet/components/Label';
 import Form from 'grommet/components/Form';
 import FormField from 'grommet/components/FormField';
 import TextInput from 'grommet/components/TextInput';
 import Button from 'grommet/components/Button';
+import Anchor from 'grommet/components/Anchor';
+import Menu from 'grommet/components/Menu';
 import {NotificationCard, TextCard, TabCard, Charts} from '../components/theme-demo';
 import StandardPage from './standard';
 import navigation from '../navigation';
@@ -26,13 +25,14 @@ export class UserSettingsPage extends Component {
       dirty: false,
       user: BrowserStorage.User
     };
-    this.onValueChange = this.onValueChange.bind(this);
+    this.onUserValueChange = this.onUserValueChange.bind(this);
     this.onBasicInfoSubmit = this.onBasicInfoSubmit.bind(this);
+    this.onThemeChange = this.onThemeChange.bind(this);
     window.BrowserStorage = BrowserStorage;
     window.cloneDeep = cloneDeep;
   }
 
-  onValueChange(event) {
+  onUserValueChange(event) {
     let fieldName = event.target.id;
     let fieldValue = event.target.value;
     this.setState((old) => {
@@ -41,6 +41,17 @@ export class UserSettingsPage extends Component {
       blah.dirty = true;
       return blah;
     });
+  }
+
+  onThemeChange(event) {
+    let fieldValue = event.target.innerText;
+    let yomama = cloneDeep(this.state.user);
+    yomama.UserPreferences.Theme = fieldValue;
+    BrowserStorage.updateUserInfo(yomama).then((response) => {
+     this.setState(() => {
+       return {user: response.data}
+     })
+    })
   }
 
   onBasicInfoSubmit(event) {
@@ -63,50 +74,54 @@ export class UserSettingsPage extends Component {
                  src={this.state.user.AvatarUrl}
           />
         </Box>
-        <Box colorIndex="grey-2-a">
-          <Accordion active={0} openMulti={true}>
-            <AccordionPanel true heading="Basic Info">
+        <Columns size="small">
+          <Menu responsive={true}
+                label='Change Theme'
+                inline={false}
+                primary={true}
+                colorIndex="grey-1-a"
+                size='medium'>
 
-              <Form plain={true} onSubmit={this.onBasicInfoSubmit}>
-                <Columns masonry={true} maxCount={8}>
-                  <FormField label="First Name" htmlFor="firstname">
-                    <TextInput id="GivenName" onDOMChange={this.onValueChange} value={this.state.user.GivenName}/>
-                  </FormField>
-                  <FormField label="Last Name" htmlFor="lastname">
-                    <TextInput id="FamilyName" onDOMChange={this.onValueChange} value={this.state.user.FamilyName}/>
-                  </FormField>
-                  <FormField label="Email (can't change)" htmlFor="EmailAddress">
-                    <TextInput id="EmailAddress" value={this.state.user.EmailAddress}/>
-                  </FormField>
-                  <FormField label="Avatar URL" htmlFor="avatarurl">
-                    <TextInput id="AvatarUrl" onDOMChange={this.onValueChange} value={this.state.user.AvatarUrl}/>
-                  </FormField>
-                  <FormField label="Job Title" htmlFor="JobTitle">
-                    <TextInput id="JobTitle" onDOMChange={this.onValueChange} value={this.state.user.JobTitle}/>
-                  </FormField>
-                </Columns>
-                <Box pad="small" align="center">
-                  <Animate visible={this.state.dirty} keep={true}>
-                    <Button label="Save" type="submit"/>
-                  </Animate>
-                </Box>
-              </Form>
-            </AccordionPanel>
-            <AccordionPanel heading="Favorites">
-              <Paragraph>
-                List of favorites will go here
-              </Paragraph>
-            </AccordionPanel>
-            <AccordionPanel heading='Slick Theme'>
-              <Columns>
-                <NotificationCard/>
-                <TextCard/>
-                <TabCard/>
-              </Columns>
-              <Charts/>
-            </AccordionPanel>
-          </Accordion>
-        </Box>
+            {Object.keys(SlickThemes).map((theme) => {
+              return (
+              <Anchor
+                className='active'
+                onClick={this.onThemeChange}>
+                {theme}
+              </Anchor>
+              );
+
+            })}
+          </Menu>
+
+        </Columns>
+        <Columns>
+          <Box colorIndex="grey-1-a">
+
+            <Form plain={true} onSubmit={this.onBasicInfoSubmit}>
+              <FormField label="First Name" htmlFor="firstname">
+                <TextInput id="GivenName" onDOMChange={this.onUserValueChange} value={this.state.user.GivenName}/>
+              </FormField>
+              <FormField label="Last Name" htmlFor="lastname">
+                <TextInput id="FamilyName" onDOMChange={this.onUserValueChange} value={this.state.user.FamilyName}/>
+              </FormField>
+              <FormField label="Email (can't change)" htmlFor="EmailAddress">
+                <TextInput id="EmailAddress" value={this.state.user.EmailAddress}/>
+              </FormField>
+              <FormField label="Avatar URL" htmlFor="avatarurl">
+                <TextInput id="AvatarUrl" onDOMChange={this.onUserValueChange} value={this.state.user.AvatarUrl}/>
+              </FormField>
+              <FormField label="Job Title" htmlFor="JobTitle">
+                <TextInput id="JobTitle" onDOMChange={this.onUserValueChange} value={this.state.user.JobTitle}/>
+              </FormField>
+              <Box pad="small" align="center">
+                <Animate visible={this.state.dirty} keep={true}>
+                  <Button label="Save" type="submit"/>
+                </Animate>
+              </Box>
+            </Form>
+          </Box>
+        </Columns>
       </StandardPage>
     );
   }
