2abc9f02ed1fa2e52a117ca4ac15b584a4d3aab5
Jason Corbett <jasoncorbett@gmail.com>
2018-03-19 11:03:09 -0600

    fixing tmux script

--- tmux-run-dev.sh
+++ tmux-run-dev.sh
@@ -2,12 +2,16 @@
 
 BASEDIR=$(dirname $(readlink -f $0))
 
-tmux new-session -s slick-dev-server -d "cd $BASEDIR; go build; ./slick serve -dev"
-
-tmux split-window -t slick-dev-server "cd $BASEDIR/web; npm install; npm run dev;"
-
+tmux new-session -t slick-dev-server -d 
 tmux set-option -t slick-dev-server mouse on
+tmux send -t slick-dev-server "cd $BASEDIR; go build; ./slick serve -dev"
+tmux send -t slick-dev-server Enter
+
+
+tmux split-window -t slick-dev-server
+
+tmux send -t slick-dev-server "cd $BASEDIR/web; npm install; npm run dev;"
+tmux send -t slick-dev-server Enter
 
 tmux rename-window -t slick-dev-server "Slick Dev Server"
-
 tmux attach -t slick-dev-server
